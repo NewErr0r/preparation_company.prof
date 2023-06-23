5af2034a7070f0e39fe3d3a780587e23444b453e
Dima <96333513+NewErr0r@users.noreply.github.com>
2023-06-23 18:42:56 +0300

    Update db_table.php

--- db_table.php
+++ db_table.php
@@ -5,7 +5,7 @@ $database = "test_db";
 $table = "db_table";
 
 try {
-  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  $db = new PDO("mysql:host=localhost;port=3306;dbname=$database", $user, $password);
   echo "<h2>TABLE</h2><ol>";
   foreach($db->query("SELECT content FROM $table") as $row) {
     echo "<li>" . $row['content'] . "</li>";
